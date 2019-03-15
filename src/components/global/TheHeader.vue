07d55b157606bbb7cb1e6365d9ef1d41641c5eeb
laifeipeng <1020103647@qq.com>
2019-03-15 20:42:33 +0800

    1、完成header和tab

--- src/components/global/TheHeader.vue
+++ src/components/global/TheHeader.vue
@@ -1,41 +1,55 @@
 <template>
   <div class="the-header">
-    
+    <div class="icon"></div>
+    <h1 class="text">Lypeng Music</h1>
+    <router-link tag="div" class="mine" to="/user">
+      <svg-icon class-name="user" icon-class="user"></svg-icon>
+    </router-link>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 export default {
   name: 'the-header',
-  props: {
-    name: {
-      type: String,
-      default: 'laifeipeng'
-    },
-    // test: {
-    //   type: String | Array,
-    //   required: true
-    // },
-  },
-  data() {
-    return {
-      show: false
-    }
-  },
-  created() {
-
-  },
-  mounted() {
-
-  },
-  methods: {
-
-  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .the-header{
-    color: blue;
+@import "~@/scss/variable";
+@import "~@/scss/mixin";
+
+.the-header {
+  position: relative;
+  height: 88px;
+  text-align: center;
+  color: $color-theme;
+  font-size: 0;
+  .icon {
+    display: inline-block;
+    vertical-align: top;
+    margin-top: 20px;
+    width: 46px;
+    height: 46px;
+    margin-right: 18px;
+    background-image: url('../../assets/logo.png');
+    background-size: 46px 46px;
   }
+  .text {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 88px;
+    font-size: $font-size-large;
+  }
+  .mine {
+    position: absolute;
+    top: 0;
+    right: 0;
+    .user {
+      display: block;
+      padding: 12px;
+      font-size: 40px;
+      color: $color-theme;
+    }
+  }
+}
 </style>
--- src/components/global/TheTab.vue
+++ src/components/global/TheTab.vue
@@ -3,11 +3,11 @@
     <router-link tag="div" class="tab-item" to="/recommend">
       <span class="tab-link">推荐</span>
     </router-link>
-    <router-link tag="div" class="tab-item" to="/singer">
-      <span class="tab-link">歌手</span>
-    </router-link>
     <router-link tag="div" class="tab-item" to="/rank">
       <span class="tab-link">排行</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/singer">
+      <span class="tab-link">歌手</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/search">
       <span class="tab-link">搜索</span>
@@ -17,31 +17,7 @@
 
 <script>
 export default {
-  name: 'the-tab',
-  props: {
-    name: {
-      type: String,
-      default: 'laifeipeng'
-    },
-    // test: {
-    //   type: String | Array,
-    //   required: true
-    // },
-  },
-  data() {
-    return {
-      show: false
-    }
-  },
-  created() {
-
-  },
-  mounted() {
-
-  },
-  methods: {
-
-  }
+  name: 'the-tab'
 }
 </script>
 
@@ -49,20 +25,20 @@ export default {
 @import "~@/scss/variable";
 .the-tab {
   display: flex;
-  height: 44px;
-  line-height: 44px;
+  height: 100px;
+  line-height: 100px;
   font-size: $font-size-medium;
   .tab-item {
     flex: 1;
     text-align: center;
     .tab-link {
-      padding-bottom: 5px;
+      padding-bottom: 10px;
       color: $color-text-l;
     }
     &.router-link-active {
       .tab-link {
         color: $color-theme;
-        border-bottom: 2px solid $color-theme;
+        border-bottom: 4px solid $color-theme;
       }
     }
   }
