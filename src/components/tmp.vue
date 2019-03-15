64d3f70a9fe29d3a7dcc76d842a3c469fa4f373f
laifeipeng <1020103647@qq.com>
2019-03-15 15:12:42 +0800

    1、统一图标名称 2、使用全局svg-icon封装

---
+++ src/components/tmp.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,30 @@
   <div class="about">
     <h1>This is an about page</h1>
     <svg class="icon" aria-hidden="true">
-    <use xlink:href="#icon-play"></use>
-</svg>
+      <use xlink:href="#icon-24gl-arrowLeftBack"></use>
+    </svg>
+    <hr>
+    <svg-icon class-name="test" icon-class="arrowLeftBack"></svg-icon>
   </div>
 </template>
+<script>
+import SvgIcon from '@/components/global/SvgIcon.vue'
+export default {
+  components: {
+    SvgIcon
+  }
+}
+</script>
+
 <style lang="scss" scoped>
-.icon{
-  color: red;  
-  fill:rgba(0,0,0,.5);
-  // font-size: 2rem;
+.icon {
+  color: red;
+  fill: rgba(0, 0, 0, 0.5);
+  font-size: 2rem;
+}
+.test {
+  fill: #f3f;
+  color: blue;
+  font-size: 2rem;
 }
 </style>
